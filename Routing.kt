37ab4fecf7085a9fb3f5e60f79b84d8b74bdf6a1
Jager1026 <74218548+Jager1026@users.noreply.github.com>
2022-06-05 22:25:03 -0500

    Update Routing.kt

--- Routing.kt
+++ Routing.kt
@@ -1,6 +1,6 @@
 package com.example.plugins
 
-import com.mongodb.client.FindIterable
+//import com.mongodb.client.FindIterable
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -8,8 +8,13 @@ import io.ktor.server.response.*
 import io.ktor.server.request.*
 import org.litote.kmongo.*
 
+//Data class for boards
+data class Boards(val name:String,val FBQN:String )
+//Data class for arduino info
+data class Arduino(val address: String, val Protocol:String, val protocol_label:String,val boards: Array<Boards> )
+
 data class User(val name:String, val pass: String, val _id:String?=null)
-data class File(val name:String, val content:String, val _id:String?=null, val owner:String?=null)
+data class File(val name:String, val fileContent:String, val _id:String?=null, val owner:String?=null)
 
 const val imadDB="imad-db-1-2022"
 
@@ -57,101 +62,149 @@ fun Application.configureRouting() {
             }
         }
 
-        post("/create/file?auth={UserID}"){
+        post("/create/file"){
 
             val parameters:Parameters = call.receiveParameters()
 
             val userID = call.parameters["UserID"]
             val fileName = parameters["file"]!!
+            val currId = call.request.queryParameters["auth"]
 
             val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
             val database = client.getDatabase(imadDB) //normal java driver usage
             val colFile = database.getCollection<File>() //KMongo extension method
+            val colUser = database.getCollection<User>()
 
-            val curFile: File? = colFile.findOne(File::name eq fileName)
+            val currUser : User? = colUser.findOne(User::_id eq currId)
+            //val curFile: File? = colFile.findOne(File::name eq fileName)
 
-            if(curFile==null){
-
-                //Creates new User object and inserts it in database
-                val insertFile=File(fileName, content = "",owner = userID)
-                colFile.insertOne(insertFile)
-
+            if(currUser == null){
                 client.close()
-
-                //Sends user ID (API key) to client
-                call.respondText(insertFile._id!!, contentType = ContentType.Text.Plain)
-            }else
-            {
-                client.close()
-
-                //Sends response to client if user already exists
-                call.respondText("File already exists", contentType = ContentType.Text.Plain)
+                call.respondText("You are not authorized", contentType = ContentType.Text.Plain)
             }
 
+            else {
+                val curFile : File? = colFile.findOne(File::name eq fileName)
+
+                if (curFile == null){
+                    val insertFile=File(fileName, fileContent = "",owner = userID)
+                    colFile.insertOne(insertFile)
+                    call.respondText( insertFile._id!! ,contentType=ContentType.Text.Plain )
+                    client.close()
+                }
+                else {
+                    client.close()
+
+                    //Sends response to client if user already exists
+                    call.respondText("File already exists", contentType = ContentType.Text.Plain)
+                }
+            }
         }
 
-        put ("/file/{fileID}?auth={UserID}"){
+        put ("/file/{fileID}"){
 
             val parameters:Parameters = call.receiveParameters()
 
             val userID = call.parameters["UserID"]
             val fileID = call.parameters["fileID"]
+            val currId = call.request.queryParameters["auth"]
 
             val contents = parameters["fileContents"]!!
 
             val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
             val database = client.getDatabase(imadDB) //normal java driver usage
             val colFile = database.getCollection<File>() //KMongo extension method
+            val colUser = database.getCollection<User>()
 
-            val curFile: File? = colFile.findOne(File::_id eq fileID)
-            val curUser: File? = colFile.findOne(File::owner eq userID)
+            val currUser : User? = colUser.findOne(User::_id eq currId)
+            //val curFile: File? = colFile.findOne(File::name eq fileName)
 
-            if(curFile != null && curUser != null){
-
-                //Creates new File object
-                val editFile=File(fileID.toString(), contents)
-                colFile.updateOne(File::_id eq fileID, set (curFile::content setTo contents))
+            //Verificamos si el usuario existe
+            if(currUser == null){
                 client.close()
+                call.respondText("You are not authorized", contentType = ContentType.Text.Plain)
+            }
 
-                //Sends user ID (API key) to client
-                call.respondText(editFile._id!!, contentType = ContentType.Text.Plain)
-            }else
-            {
-                client.close()
+            else {
+                //Verificamos que el archivo existe
+                val curFile: File? = colFile.findOne(File:: _id eq fileID)
 
-                //Sends response to client if user already exists
-                call.respondText("Wrong File", contentType = ContentType.Text.Plain)
+                if(curFile == null){
+
+                    call.respondText("Wrong File", contentType = ContentType.Text.Plain)
+                }
+
+                else{
+                    
+                    //Verificamos si el archivo le pertenece al usuario
+                    
+                    if (curFile.owner == currUser._id) {
+
+                        val editFile = File(fileID.toString(), contents)
+                        colFile.updateOne(File::_id eq fileID, set(curFile::fileContent setTo contents))
+                        client.close()
+                    
+                        //Sends user ID (API key) to client
+                        call.respondText(editFile._id!!, contentType = ContentType.Text.Plain)
+                        call.respondText("OK", contentType = ContentType.Text.Plain)
+                    }
+                    else{
+                        call.respondText("Wrong File", contentType = ContentType.Text.Plain)
+                    }
+                }
             }
         }
 
-        get ("/file/{fileID}?auth={userID}") {
+        get ("/files/{fileID}") {
 
-            val userId = call.parameters["userID"]
+            val parameters:Parameters = call.receiveParameters()
+
+            val userID = call.parameters["UserID"]
             val fileID = call.parameters["fileID"]
+            val currId = call.request.queryParameters["auth"]
 
             val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
             val database = client.getDatabase(imadDB) //normal java driver usage
             val colFile = database.getCollection<File>() //KMongo extension method
+            val colUser = database.getCollection<User>()
 
-            val curFile: File? = colFile.findOne(File::_id eq fileID)
-            val curUser: File? = colFile.findOne(File::owner eq userId)
+            val currUser : User? = colUser.findOne(User::_id eq currId)
 
-            if(curFile != null && curUser != null){
+            //Verificamos que el usuario tiene autorizacion
 
-                call.respondText(curFile.content, contentType = ContentType.Text.Plain)
+            if(currUser == null){
+
+                call.respondText("You are not authorized", contentType = ContentType.Text.Plain)
                 client.close()
 
-            }else
-            {
-                client.close()
-
-                //Sends response to client if user already exists
-                call.respondText("Wrong FileID", contentType = ContentType.Text.Plain)
             }
+            else{
+                
+                //Verificamos que el archivo existe
+                val curFile: File? = colFile.findOne(File:: _id eq fileID)
 
+                if(curFile == null){
+                    call.respondText("Wrong File", contentType = ContentType.Text.Plain)
+                    client.close()
+                }
+                else{
+                    
+                    //Verificamos que el archivo pertenezca al usuario
+                    
+                    if (curFile.owner == currUser._id) {
+                        call.respondText(curFile.fileContent, contentType = ContentType.Text.Plain)
+                        client.close() 
+                    }
+                    else{
+                            call.respondText("Wrong file", contentType = ContentType.Text.Plain)
+                            client.close()
+                    }
+                    
+                }
+            }
         }
 
-        get("/files?auth={UserID}"){
+        get("/files/{UserID}"){
 
             val userId = call.parameters["UserID"]
 
@@ -160,17 +213,141 @@ fun Application.configureRouting() {
             val colFile = database.getCollection<File>() //KMongo extension method
 
             //val curFile: MutableList<File> = colFile.find(File::owner eq userId).toMutableList()
-            val curFile: Iterable<File> = colFile.find(File::owner eq userId).toMutableList()
+            //val curFile: Iterable<File> = colFile.find(File::owner eq userId).toMutableList()
+            val listOfFiles = colFile.find(File:: owner eq userId).toMutableList()
 
-            if (curFile != null){
+            if (listOfFiles != null){
 
                 call.respondText(curFile.toString(), contentType = ContentType.Text.Plain)
 
             }
 
             else{
+                
                 call.respondText("There arent files owned by this user", contentType = ContentType.Text.Plain)
             }
+
+        }
+
+        get("/flash/file/{fileid}") {
+
+            //Reads auth (API key) from URI
+            val currId = call.request.queryParameters["auth"]
+
+            //Reads file ID from URI
+            val fileID=call.parameters["fileid"]
+
+            //Auxiliary objects to manage MongoDB database
+            val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
+            val database = client.getDatabase(imadDB) //normal java driver usage
+            val colUser = database.getCollection<User>() //KMongo extension method
+            val colFile = database.getCollection<File>() //KMongo extension method
+
+            //Checks if user already exists
+            val curUser: User? = colUser.findOne(User::_id eq currId)
+
+            //If user does not exist
+            if(curUser==null){
+                client.close()
+                //Sends response to client
+                call.respondText("You are not authorized", contentType = ContentType.Text.Plain)
+
+            }else {
+
+                //User is authorized, then finds requested file
+                val curFile = colFile.findOne(File::_id eq fileID)
+
+
+                //If requested file does not exist
+                if (curFile == null) {
+
+                    //Sends response to client
+                    call.respondText("Wrong file", contentType = ContentType.Text.Plain)
+                    client.close()
+
+
+
+                } else {
+                    //File exists, then checks if it belongs to current user
+                    if (curFile.owner == curUser._id) {
+
+                        val dir = java.io.File(curFile.name.substringBefore('.'));
+                        dir.mkdir()
+
+                        val file=java.io.File(dir,curFile.name)
+
+                        val fos = FileOutputStream(file)
+
+                        fos.write(curFile.fileContent.toByteArray())
+
+
+                        fos.close()
+
+                        var process =
+
+                            ProcessBuilder("arduino-cli", "board", "list", "--format","json").start()
+                        var stdout=process.inputStream
+
+                        var isstdout=InputStreamReader(stdout)
+
+                        var brstdout=BufferedReader(isstdout)
+
+                        process.waitFor()
+
+                        var response=brstdout.readText()
+
+                        val obj=Gson().fromJson(response,Array<Arduino>::class.java)
+
+                        val arduino=obj.filter { it ->
+                            if(it.boards!=null) {
+                                val device= it.boards.filter { it ->
+
+                                    it.name.equals("Arduino Uno")
+
+                                }
+
+                                device!=null
+
+                            }else
+                                false
+                        }
+
+                        var port="COM1"
+                        if(arduino.size!=0)
+                            port=arduino[0].address;
+
+                        process =
+                            ProcessBuilder("arduino-cli", "compile", "-b", "arduino:avr:uno","-u", "-p",
+                                port, curFile.name.substringBefore('.')).start()
+                        var errs=process.errorStream
+
+                        var isrerr = InputStreamReader(errs)
+
+                        var brerr=BufferedReader(isrerr)
+
+                        process.waitFor()
+
+                        response=brerr.readText()
+
+                        response =if(response.isEmpty()) "Sketch was successfully flashed\n" else response
+
+                            //Sends response from Arduino CLI
+                        call.respondText(response, contentType = ContentType.Text.Plain)
+
+                        client.close()
+
+                    } else {
+                        //Even though file exists, it does not belong to current user
+                        call.respondText("Wrong file", contentType = ContentType.Text.Plain)
+                        client.close()
+
+                    }
+
+
+                }
+
+            }
+
 
         }
         
